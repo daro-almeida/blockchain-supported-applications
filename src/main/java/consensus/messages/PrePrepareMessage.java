440d2b3606820657b8f6ecda37f953453f67e0f8
Jaime Saramago <jm.saramago@campus.fct.unl.pt>
2023-03-21 14:35:18 +0000

    preprepare content

--- src/main/java/consensus/messages/PrePrepareMessage.java
+++ src/main/java/consensus/messages/PrePrepareMessage.java
@@ -5,28 +5,59 @@ import java.io.IOException;
 import io.netty.buffer.ByteBuf;
 import pt.unl.fct.di.novasys.babel.generic.signed.SignedMessageSerializer;
 import pt.unl.fct.di.novasys.babel.generic.signed.SignedProtoMessage;
+import pt.unl.fct.di.novasys.network.data.Host;
 
 public class PrePrepareMessage extends SignedProtoMessage {
 
 	public final static short MESSAGE_ID = 101;
 
+	private int viewN, seq;
+	private byte[] digest;
+	private Host sender;
+
 	public PrePrepareMessage() {
 		super(PrePrepareMessage.MESSAGE_ID);
+	}
+
+	public int getViewN() {
+		return viewN;
+	}
+
+	public int getSeq() {
+		return seq;
+	}
+
+	public byte[] getDigest() {
+		return digest;
+	}
+
+	public Host getSender() {
+		return sender;
 	}
 
 	public static final SignedMessageSerializer<PrePrepareMessage> serializer = new SignedMessageSerializer<PrePrepareMessage>() {
 
 		@Override
 		public void serializeBody(PrePrepareMessage signedProtoMessage, ByteBuf out) throws IOException {
-
+			out.writeInt(signedProtoMessage.viewN);
+			out.writeInt(signedProtoMessage.seq);
+			out.writeInt(signedProtoMessage.digest.length);
+			out.writeBytes(signedProtoMessage.digest); 
+			Host.serializer.serialize(signedProtoMessage.sender, out);
 		}
 
 		@Override
 		public PrePrepareMessage deserializeBody(ByteBuf in) throws IOException {
-			return null;
+			PrePrepareMessage msg = new PrePrepareMessage();
+            msg.viewN = in.readInt();
+            msg.seq = in.readInt();
+            msg.digest = new byte[in.readInt()];
+            in.readBytes(msg.digest);
+            msg.sender = Host.serializer.deserialize(in);
+            return msg;
 		}
 	};
-	
+
 	@Override
 	public SignedMessageSerializer<? extends SignedProtoMessage> getSerializer() {
 		return PrePrepareMessage.serializer;
