7b4cd050132b7e9694154f3956551167c73ed8ff
daro.almeida <daro.almeida@campus.fct.unl.pt>
2023-03-29 21:10:18 +0100

    added skeleton to messages

--- src/main/java/blockchain/messages/ClientRequestUnhandledMessage.java
+++ src/main/java/blockchain/messages/ClientRequestUnhandledMessage.java
@@ -1,8 +1,19 @@
 package blockchain.messages;
 
-public class ClientRequestUnhandledMessage {
+import pt.unl.fct.di.novasys.babel.generic.signed.SignedMessageSerializer;
+import pt.unl.fct.di.novasys.babel.generic.signed.SignedProtoMessage;
+
+public class ClientRequestUnhandledMessage extends SignedProtoMessage {
 
 	public final static short MESSAGE_ID = 202;
-	
 
+	public ClientRequestUnhandledMessage() {
+		super(MESSAGE_ID);
+	}
+
+
+	@Override
+	public SignedMessageSerializer<? extends SignedProtoMessage> getSerializer() {
+		return null;
+	}
 }
--- src/main/java/blockchain/messages/StartClientRequestSuspectMessage.java
+++ src/main/java/blockchain/messages/StartClientRequestSuspectMessage.java
@@ -1,7 +1,18 @@
 package blockchain.messages;
 
-public class StartClientRequestSuspectMessage {
+import pt.unl.fct.di.novasys.babel.generic.signed.SignedMessageSerializer;
+import pt.unl.fct.di.novasys.babel.generic.signed.SignedProtoMessage;
+
+public class StartClientRequestSuspectMessage extends SignedProtoMessage {
 
 	public final static short MESSAGE_ID = 203;
-	
+
+	public StartClientRequestSuspectMessage() {
+		super(MESSAGE_ID);
+	}
+
+	@Override
+	public SignedMessageSerializer<? extends SignedProtoMessage> getSerializer() {
+		return null;
+	}
 }
