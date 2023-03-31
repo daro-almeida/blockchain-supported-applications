5a4d89525ebd1eaf5b4b6658cd421d42e2125896
Jaime Saramago <jm.saramago@campus.fct.unl.pt>
2023-03-31 15:49:31 +0100

    messages completed (ig)

--- src/main/java/blockchain/messages/ClientRequestUnhandledMessage.java
+++ src/main/java/blockchain/messages/ClientRequestUnhandledMessage.java
@@ -1,33 +1,60 @@
 package blockchain.messages;
 
+import java.io.IOException;
+
+import blockchain.requests.ClientRequest;
 import io.netty.buffer.ByteBuf;
 import pt.unl.fct.di.novasys.babel.generic.signed.SignedMessageSerializer;
 import pt.unl.fct.di.novasys.babel.generic.signed.SignedProtoMessage;
-
-import java.io.IOException;
+import utils.Utils;
 
 //TODO implement this message
 public class ClientRequestUnhandledMessage extends SignedProtoMessage {
 
 	public final static short MESSAGE_ID = 202;
 
-	public ClientRequestUnhandledMessage() {
-		super(MESSAGE_ID);
+	private final ClientRequest request;
+	private final byte[] signature; // this is the request's signature signed by the client
+	private final int nodeId;
+
+	public ClientRequestUnhandledMessage(ClientRequest request, byte[] signature, int nodeId) {
+		super(ClientRequestUnhandledMessage.MESSAGE_ID);
+		this.request = request;
+		this.signature = signature;
+		this.nodeId = nodeId;
+	}
+
+	public ClientRequest getRequest() {
+		return request;
+	}
+
+	public byte[] getSignature() {
+		return signature;
+	}
+
+	public int getNodeId() {
+		return nodeId;
 	}
 
 	public static final SignedMessageSerializer<ClientRequestUnhandledMessage> serializer = new SignedMessageSerializer<>() {
 
 		@Override
 		public void serializeBody(ClientRequestUnhandledMessage protoMessage, ByteBuf out) throws IOException {
+			Utils.byteArraySerializer.serialize(protoMessage.request.generateByteRepresentation(), out);
+			Utils.byteArraySerializer.serialize(protoMessage.signature, out);
+			out.writeInt(protoMessage.nodeId);
 
 		}
 
 		@Override
 		public ClientRequestUnhandledMessage deserializeBody(ByteBuf in) throws IOException {
-			return new ClientRequestUnhandledMessage();
+			byte[] requestBytes = Utils.byteArraySerializer.deserialize(in);
+			byte[] signature = Utils.byteArraySerializer.deserialize(in);
+			var request = ClientRequest.fromBytes(requestBytes);
+			int nodeId = in.readInt();
+			return new ClientRequestUnhandledMessage(request, signature, nodeId);
 		}
 	};
-
 
 	@Override
 	public SignedMessageSerializer<? extends SignedProtoMessage> getSerializer() {
--- src/main/java/blockchain/messages/StartClientRequestSuspectMessage.java
+++ src/main/java/blockchain/messages/StartClientRequestSuspectMessage.java
@@ -3,6 +3,7 @@ package blockchain.messages;
 import io.netty.buffer.ByteBuf;
 import pt.unl.fct.di.novasys.babel.generic.signed.SignedMessageSerializer;
 import pt.unl.fct.di.novasys.babel.generic.signed.SignedProtoMessage;
+import utils.Utils;
 
 import java.io.IOException;
 
@@ -11,20 +12,45 @@ public class StartClientRequestSuspectMessage extends SignedProtoMessage {
 
 	public final static short MESSAGE_ID = 203;
 
-	public StartClientRequestSuspectMessage() {
-		super(MESSAGE_ID);
+	private final short requestId; 
+	private final byte[] signature; // this is the request's signature signed by the client
+	private final int nodeId;
+
+	public StartClientRequestSuspectMessage(short requestId, byte[] signature, int nodeId) {
+		super(StartClientRequestSuspectMessage.MESSAGE_ID);
+		this.requestId = requestId;
+		this.signature = signature;
+		this.nodeId = nodeId;
+	}
+
+	public short getRequestId() {
+		return requestId;
+	}
+
+	public byte[] getSignature() {
+		return signature;
+	}
+
+	public int getNodeId() {
+		return nodeId;
 	}
 
 	public static final SignedMessageSerializer<StartClientRequestSuspectMessage> serializer = new SignedMessageSerializer<>() {
 
 		@Override
 		public void serializeBody(StartClientRequestSuspectMessage protoMessage, ByteBuf out) throws IOException {
+			out.writeShort(protoMessage.requestId);
+			Utils.byteArraySerializer.serialize(protoMessage.signature, out);
+			out.writeInt(protoMessage.nodeId);
 
 		}
 
 		@Override
 		public StartClientRequestSuspectMessage deserializeBody(ByteBuf in) throws IOException {
-			return new StartClientRequestSuspectMessage();
+			short id = in.readShort();
+			byte[] signature = Utils.byteArraySerializer.deserialize(in);
+			int nodeId = in.readInt();
+			return new StartClientRequestSuspectMessage(id, signature, nodeId);
 		}
 	};
 
