2cb2ffa468089645ac7adbd4c822249048280547
Jaime Saramago <jm.saramago@campus.fct.unl.pt>
2023-05-28 17:29:01 +0100

    toolbox skeleton

--- server/src/main/java/app/toolbox/Toolbox.java
+++ server/src/main/java/app/toolbox/Toolbox.java
@@ -1,19 +1,34 @@
 package app.toolbox;
 
-import blockchain.BlockChainProtocol;
-import consensus.PBFTProtocol;
-import metrics.Metrics;
+import java.io.IOException;
+import java.security.PublicKey;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import java.util.UUID;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import app.WriteOperation;
+import app.open_goods.Destination;
+import app.open_goods.messages.client.replies.GenericClientReply;
+import app.open_goods.messages.client.replies.OperationStatusReply;
+import app.open_goods.messages.client.replies.OperationStatusReply.Status;
+import blockchain.BlockChainProtocol;
+import blockchain.notifications.ExecutedOperation;
+import consensus.PBFTProtocol;
+import metrics.Metrics;
 import pt.unl.fct.di.novasys.babel.core.Babel;
 import pt.unl.fct.di.novasys.babel.core.GenericProtocol;
 import pt.unl.fct.di.novasys.babel.exceptions.HandlerRegistrationException;
 import pt.unl.fct.di.novasys.channel.simpleclientserver.SimpleServerChannel;
+import pt.unl.fct.di.novasys.channel.simpleclientserver.events.ClientDownEvent;
+import pt.unl.fct.di.novasys.channel.simpleclientserver.events.ClientUpEvent;
+import utils.Crypto;
 import utils.Utils;
-
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Properties;
 
 public class Toolbox extends GenericProtocol {
 
@@ -24,6 +39,17 @@ public class Toolbox extends GenericProtocol {
 
     public final static String PROTO_NAME = "Toolbox";
     public final static short PROTO_ID = 600;
+
+    // incoming polls
+    private final Map<UUID, Poll> polls = new HashMap<>();
+
+    // TODO: change votes uuid to cote obj.
+    private final Map<UUID, Set<UUID>> votes = new HashMap<>();
+
+    // to send reply later
+    private final Map<UUID, Destination> reqDestinations = new HashMap<>();
+
+    private final Map<UUID, OperationStatusReply.Status> opStatus = new HashMap<>();
 
     private int clientChannel;
 
@@ -63,13 +89,98 @@ public class Toolbox extends GenericProtocol {
     }
 
     @Override
-    public void init(Properties  props) throws HandlerRegistrationException, IOException {
+    public void init(Properties props) throws HandlerRegistrationException, IOException {
         Properties serverProps = new Properties();
         serverProps.put(SimpleServerChannel.ADDRESS_KEY, props.getProperty(ADDRESS_KEY));
         serverProps.setProperty(SimpleServerChannel.PORT_KEY, props.getProperty(SERVER_PORT_KEY));
 
         clientChannel = createChannel(SimpleServerChannel.NAME, serverProps);
+        // register stuff here
 
-        //register stuff here
+        // registermsgserialzer from clients
+
+        // handlers
+
+        // Handle exec. operation
+        registerChannelEventHandler(clientChannel, ClientUpEvent.EVENT_ID, this::uponClientConnectionUp);
+        registerChannelEventHandler(clientChannel, ClientDownEvent.EVENT_ID, this::uponClientConnectionDown);
+
+        subscribeNotification(ExecutedOperation.NOTIFICATION_ID, this::handleExecutedOperation);
+    }
+
+    /*
+     * NOTIFICATIONS
+     */
+
+    private void handleExecutedOperation(ExecutedOperation notif, short sourceProto) {
+
+        assert (opStatus.get(notif.getRequest().getRequestId()) == Status.PENDING ||
+                opStatus.get(notif.getRequest().getRequestId()) == null);
+
+        var operation = WriteOperation.fromBytes(notif.getRequest().getOperation());
+        switch (operation.getId()) {
+            //TODO: pollmessgs
+            /* case IssueOffer.MESSAGE_ID -> handleExecutedIssueOffer((IssueOffer) operation);
+            case IssueWant.MESSAGE_ID -> handleExecutedIssueWant((IssueWant) operation);
+            case Cancel.MESSAGE_ID -> handleExecutedCancel((Cancel) operation);
+            case Deposit.MESSAGE_ID -> handleExecutedDeposit((Deposit) operation);
+            case Withdrawal.MESSAGE_ID -> handleExecutedWithdrawal((Withdrawal) operation); */
+            default -> logger.error("Received unknown operation for open goods market");
+        }
+
+    }
+
+    /*
+     * CONNECTION EVENTS
+     */
+
+    private void uponClientConnectionUp(ClientUpEvent event, int channel) {
+        logger.debug(event);
+    }
+
+    private void uponClientConnectionDown(ClientDownEvent event, int channel) {
+        logger.warn(event);
+    }
+
+    private boolean repeatedOperation(UUID requestId, Destination dest) {
+        if (opStatus.containsKey(requestId) && opStatus.get(requestId) != OperationStatusReply.Status.PENDING) {
+            logger.warn("Repeated operation {}", requestId);
+            opStatus.put(requestId, Status.FAILED);
+            sendStatus(requestId, Status.FAILED, dest);
+            return true;
+        }
+        return false;
+    }
+
+    private void sendStatus(UUID requestId, Status status, Destination dest) {
+        if (dest == null)
+            return;
+        OperationStatusReply statusReply = new OperationStatusReply(requestId, status);
+        sendMessage(clientChannel, statusReply, dest.sourceProto(), dest.host(), 0);
+    }
+
+    private void sendAck(UUID requestId, Destination dest) {
+        assert dest != null;
+        GenericClientReply ack = new GenericClientReply(requestId);
+        sendMessage(clientChannel, ack, dest.sourceProto(), dest.host(), 0);
+    }
+
+    private void changeAndNotifyStatus(UUID requestId, Status status) {
+        opStatus.put(requestId, status);
+        var hostToReply = reqDestinations.get(requestId);
+        if (hostToReply != null) {
+            sendStatus(requestId, status, hostToReply);
+            reqDestinations.remove(requestId);
+        }
+    }
+
+    private boolean authenticatedOperation(WriteOperation op, UUID requestId, PublicKey key, Destination dest) {
+        if (!Crypto.checkSignature(op, key)) {
+            logger.warn("Invalid signature in operation from client {}", dest.host());
+            opStatus.put(requestId, Status.REJECTED);
+            sendStatus(requestId, Status.REJECTED, dest);
+            return false;
+        }
+        return true;
     }
 }
